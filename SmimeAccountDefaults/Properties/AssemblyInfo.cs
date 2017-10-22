96cae727d9834399f23b67e1dc444f63eafac6fe
Oren Novotny <oren@novotny.org>
2017-10-22 14:47:50 -0400

    Add versioning

--- SmimeAccountDefaults/Properties/AssemblyInfo.cs
+++ SmimeAccountDefaults/Properties/AssemblyInfo.cs
@@ -23,16 +23,3 @@ using System.Security;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b5a6393a-ffba-4b7e-a485-7f278e215a1b")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
-
